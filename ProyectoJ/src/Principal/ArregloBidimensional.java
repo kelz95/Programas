efc976f8f3f138734ecca20c7b9dab3bdf980c05
Alumno <Alumno@maq7>
2018-05-12 13:17:32 -0500

    Proyecto de Raquel

--- ProyectoJ/src/Principal/ArregloBidimensional.java
+++ ProyectoJ/src/Principal/ArregloBidimensional.java
@@ -13,25 +13,15 @@ public class ArregloBidimensional {
      public static void main(String[] args) {
         int [][] arregloEntero = new int [10][10];
         int n = 1;
-        int i, j;
-        for(i=0; i<10; i++)
+        for(int i=0; i<10; i++)
         {
-            for (j=0; j<10; j++)
+            for (int j=0; j<10; j++)
             {
-                arregloEntero[i][j]=n;
-                n++;
-                System.out.print("\t" + arregloEntero[i][j]);
+            arregloEntero[i][j]=n;
+            n++;
+            System.out.print("\t" + arregloEntero[i][j]);
             }
-        for (j=j; j<10; j++)
-        { 
-            for(i=i+1; i<10; i++)
-            {
-                  
-                    arregloEntero[i][j]=n;
-                n++;
-                System.out.print("\t" + arregloEntero[i][j]);
-                }
-            }
+        
         System.out.println("");
         }
      }
