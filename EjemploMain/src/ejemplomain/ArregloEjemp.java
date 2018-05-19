f16c7c6f24ea9726539f3b35ca2a944ccf7cdfcc
Alumno <Alumno@maq7>
2018-05-19 10:09:40 -0500

    Proyecto de Raquel

---
+++ EjemploMain/src/ejemplomain/ArregloEjemp.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplomain;
+
+/**
+ *
+ * @author Alumno
+ */
+public class ArregloEjemp {
+    public static void main(String[] args) {
+        char [][] tringulo2 = new char [10][10];
+         for(int x=0; x<10; x++)
+           {
+               for (int y=0; y<=x; y++)
+               {
+                   //System.out.print("\t *");
+                   tringulo2[x][y] = '*';
+                   //System.out.print("\t" + arregloEntero[x][y]);
+                   //System.out.print(x);
+                   //System.out.print(y); 
+               }
+               //System.out.println("");
+           }     
+           for(int i=0; i<10; i++)
+           {
+               for (int j=0; j<10; j++)
+               {
+                   System.out.print("\t" + tringulo2[i][j]);
+               }
+               System.out.println("");
+           }
+     }
+}
